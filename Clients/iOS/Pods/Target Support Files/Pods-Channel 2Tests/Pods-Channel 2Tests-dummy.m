ab34195718f0a494b59278d44f102c9dbbaebd39
Brendan Manning <BrendanManning19380@gmail.com>
2016-09-28 06:29:24 -0400

    Add files via upload

---
+++ Clients/iOS/Pods/Target Support Files/Pods-Channel 2Tests/Pods-Channel 2Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Channel_2Tests : NSObject
+@end
+@implementation PodsDummy_Pods_Channel_2Tests
+@end
---
+++ Clients/iOS/Pods/Target Support Files/Pods-Channel 2Tests/Pods-Channel 2Tests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Channel_2TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Channel_2TestsVersionString[];
+
