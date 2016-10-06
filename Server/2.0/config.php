e03dcdc6aedf681c330eef836f353c3ba1201b3b
Brendan Manning <BrendanManning19380@gmail.com>
2016-10-05 21:52:51 -0400

    Add files via upload

---
+++ Server/2.0/config.php
@@ -0,0 +1,8 @@
+<?php
+	$host = "<your db host><usuall localhost>";
+	$user = "<MySQL User>";
+	$pass = "<MySQL User's pass>";
+	$name = "<Name of MySQL Database>";
+	
+	$password = "<Password for admin UI>";
+?>
