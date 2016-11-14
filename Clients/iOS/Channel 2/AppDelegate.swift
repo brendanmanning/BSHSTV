926c37e262d29d03802fb9f0587ef317ff1378d7
Brendan Manning <BrendanManning19380@gmail.com>
2016-11-14 14:14:58 -0500

    Update AppDelegate.swift

--- Clients/iOS/Channel 2/AppDelegate.swift
+++ Clients/iOS/Channel 2/AppDelegate.swift
@@ -100,17 +100,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
         if(NSUserDefaults.standardUserDefaults().objectForKey("YTKEY") == nil)
         {
-            NSUserDefaults.standardUserDefaults().setValue("{YT_KEY}", forKey: "YTKEY")
+            NSUserDefaults.standardUserDefaults().setValue("{yt_key}", forKey: "YTKEY")
         }
 
         if(NSUserDefaults.standardUserDefaults().objectForKey("API_KEY") == nil)
         {
-            NSUserDefaults.standardUserDefaults().setValue("{API_KEY}", forKey: "API_KEY")
+            NSUserDefaults.standardUserDefaults().setValue("{api_key}", forKey: "API_KEY")
         }
 
         if(NSUserDefaults.standardUserDefaults().objectForKey("API_SECRET") == nil)
         {
-            NSUserDefaults.standardUserDefaults().setValue("{API_SECRET}", forKey: "API_SECRET");
+            NSUserDefaults.standardUserDefaults().setValue("{api_secret}", forKey: "API_SECRET");
         }
 
         if(NSUserDefaults.standardUserDefaults().objectForKey("alreadyGoingToArray") == nil)
